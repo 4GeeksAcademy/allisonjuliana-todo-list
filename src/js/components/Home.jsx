622a8f0eb09076f1c8cf7ee2240177c1e1673ee2
allisonjuliana <162182668+allisonjuliana@users.noreply.github.com>
2025-01-16 21:43:07 +0000

    todo-list

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiOutlineClose } from "react-icons/ai";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const [inputValue, setInputValue] = useState("")
+  const [all, setAll] = useState([]);
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  return (
+    <div className="container">
+      <div className="titulo">
+        <p>todos</p>
+      </div>
+      <div className="list">
+        <div className="header"><input
+          type="text"
+          onChange={(e) => setInputValue(e.target.value)}
+          value={inputValue}
+          onKeyDown={(e) => {
+            if(e.key === "Enter"){
+              setAll(all.concat([inputValue]));
+              setInputValue("");
+            }
+          }}
+          placeholder="What needs to be done?"></input>
+        </div>
+        {all.map((item, index) => (
+            <div className="items">
+              {item}
+              <button key={index} onClick={() => setAll(all.filter((t, currentIndex) => index != currentIndex))}><AiOutlineClose /></button>
+            </div>
+          ))
+        }
+        <div className="footer">
+        {all.length} items left
+        </div>
+
+      </div>
+      <div className="hoja2"></div>
+      <div className="hoja3"></div>
+    </div>
+  );
 };
-
+ 
 export default Home;
